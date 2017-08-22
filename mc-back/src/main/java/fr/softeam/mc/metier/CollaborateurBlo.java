7ce891d604def239a1b7e5e08a86339097bccecb
Ricardo Lopes Silva <ricardo.lopessilva@softeam.fr>
2017-08-22 16:54:04 +0200

    Adding collaborateur resource

--- mc-back/src/main/java/fr/softeam/mc/metier/CollaborateurBlo.java
+++ mc-back/src/main/java/fr/softeam/mc/metier/CollaborateurBlo.java
@@ -1,4 +1,34 @@
 package fr.softeam.mc.metier;
 
+import fr.softeam.mc.metier.dto.CollaborateurDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class CollaborateurBlo {
+
+    public List<CollaborateurDTO> recupererCollaborateurs() {
+
+        CollaborateurDTO collaborateurDTO = new CollaborateurDTO();
+        collaborateurDTO.withNom("Lopes").withPrenom("Ricardo");
+
+        CollaborateurDTO collaborateurDTO2 = new CollaborateurDTO();
+        collaborateurDTO2.withNom("Le Galludec").withPrenom("Sébastien");
+
+        List<CollaborateurDTO> collaborateurDTOList = new ArrayList<>();
+        collaborateurDTOList.add(collaborateurDTO);
+        collaborateurDTOList.add(collaborateurDTO2);
+
+        return collaborateurDTOList;
+
+    }
+
+    public CollaborateurDTO recupererCollaborateur(final String idCollaborator) {
+        CollaborateurDTO collaborateurDTO = new CollaborateurDTO();
+        collaborateurDTO.withNom("Lopes").withPrenom("Ricardo");
+
+        return collaborateurDTO;
+    }
 }
--- mc-back/src/main/java/fr/softeam/mc/resources/CollaborateurResource.java
+++ mc-back/src/main/java/fr/softeam/mc/resources/CollaborateurResource.java
@@ -1,11 +1,30 @@
 package fr.softeam.mc.resources;
 
 
+import fr.softeam.mc.metier.CollaborateurBlo;
+import fr.softeam.mc.metier.dto.CollaborateurDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CollaborateurResource extends BaseResource{
 
+    @Autowired
+    private CollaborateurBlo collaborateurBlo;
 
+    @GetMapping(path = "/collaborateurs", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<CollaborateurDTO> recupererCollaborateurs() {
+        return collaborateurBlo.recupererCollaborateurs();
+    }
+
+    @GetMapping(path = "/collaborateurs/{id_collaborateur}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public CollaborateurDTO recupererCollaborateur(@PathVariable(name="id_collaborateur") final String idCollaborateur) {
+        return collaborateurBlo.recupererCollaborateur(idCollaborateur);
+    }
 
 }
