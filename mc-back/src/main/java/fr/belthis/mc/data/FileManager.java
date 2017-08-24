07fd02156a6f896f876b6a8e53a735464e9de9d1
Ricardo Lopes Silva <ricardo.lopessilva@softeam.fr>
2017-08-24 17:31:29 +0200

    Add FileManager with history

--- mc-back/src/main/java/fr/belthis/mc/data/FileManager.java
+++ mc-back/src/main/java/fr/belthis/mc/data/FileManager.java
@@ -1,16 +1,23 @@
 package fr.belthis.mc.data;
 
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import fr.belthis.mc.metier.dto.CollaborateurDTO;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.PathMatcher;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,6 +37,67 @@ public class FileManager {
 
 
         return listeFichiers;
+    }
+
+    public void sauvegarderCollaborateur(final CollaborateurDTO collaborateurDTO) throws IOException {
+        final String nomFichier = collaborateurDTO.getPrenom() + "_" + collaborateurDTO.getNom().replace(" ", "_") + ".json";
+
+        final Path p = Paths.get(URI.create("file://" + repertoireStockage + "/" + nomFichier));
+
+        if (Files.exists(p)) {
+            historiserFichier(p, collaborateurDTO);
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+        writer.writeValue(p.toFile(), collaborateurDTO);
+
+    }
+
+
+    private void historiserFichier(final Path fichier, final CollaborateurDTO collaborateurDTO) throws IOException {
+
+        final LocalDate now = LocalDate.now();
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        final String dateFichier = now.format(formatter);
+
+        String nouveauNom = collaborateurDTO.getPrenom() + "_" + collaborateurDTO.getNom().replace(" ", "_") + "-" + dateFichier;
+
+        final Path p = Paths.get(URI.create("file://" + repertoireStockage));
+
+        final PathMatcher filter = p.getFileSystem().getPathMatcher("regex:.*" + nouveauNom + ".*\\.json$");
+        final Stream<Path> stream = Files.list(p);
+
+        Integer maxHistorique = 0;
+
+        final OptionalInt max = stream.filter(filter::matches).mapToInt(path -> {
+            final String fileName = path.getFileName().toString();
+            if (StringUtils.countMatches(fileName, '-') > 1) {
+                return getNbHistorique(fileName);
+            } else {
+                return 0;
+            }
+        }).max();
+
+        if(max.isPresent()) {
+            maxHistorique = max.getAsInt() + 1;
+        }
+
+        if (maxHistorique != 0) {
+            nouveauNom = nouveauNom + "-" + maxHistorique + ".json";
+        } else {
+            nouveauNom = nouveauNom + ".json";
+        }
+
+        Files.move(fichier, fichier.resolveSibling(nouveauNom));
+    }
+
+    private int getNbHistorique(String fileName) {
+        final int indexOfUnderscore = StringUtils.lastIndexOf(fileName, "-");
+        final int indexOfPoint = StringUtils.lastIndexOf(fileName, ".");
+        final String historique = StringUtils.substring(fileName, indexOfUnderscore + 1, indexOfPoint);
+
+        return NumberUtils.isDigits(historique) ? Integer.valueOf(historique) : 0;
     }
 
 
--- mc-back/src/test/java/fr/belthis/mc/data/FileManagerTest.java
+++ mc-back/src/test/java/fr/belthis/mc/data/FileManagerTest.java
@@ -1,5 +1,6 @@
 package fr.belthis.mc.data;
 
+import fr.belthis.mc.metier.dto.CollaborateurDTO;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,9 +9,12 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
+import java.nio.file.*;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,6 +37,35 @@ public class FileManagerTest {
 
         assertThat(files).isNotNull();
         assertThat(files.size()).isEqualTo(2);
+
+    }
+
+    @Test
+    public void sauvegarderCollaborateurTest_Nouveau() throws IOException {
+        final CollaborateurDTO collaborateurDTO = new CollaborateurDTO();
+        collaborateurDTO.withNom("LE TOTO").withPrenom("Titi").withDateModification(LocalDateTime.now());
+
+
+        target.sauvegarderCollaborateur(collaborateurDTO);
+
+        final URL stockage = ClassLoader.getSystemResource("stockage");
+
+        final Path pathStockage = Paths.get(URI.create("file://" + stockage.getPath().toString()));
+
+        final Path path1 = pathStockage.resolve("Titi_LE_TOTO.json");
+
+        assertThat(Files.exists(path1)).isTrue();
+
+        final PathMatcher filter = pathStockage.getFileSystem().getPathMatcher("regex:.*Titi_LE_TOTO.*\\.json$");
+        final Stream<Path> stream = Files.list(pathStockage);
+
+        stream.filter(filter::matches).forEach(path -> {
+            try {
+                Files.deleteIfExists(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
     }
 
