d26e9a7372d3b3c3ebc9846842127df3280e84a2
slegalludec <sebastien.legalludec@gmail.com>
2017-10-19 07:57:35 +0200

    [Init] Structuration de ihm

---
+++ mc-front/src/components/Main.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="main">
+    <v-layout column>
+        <v-flex xs12 sm12>
+          <v-toolbar color="indigo" dark>
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title>Discover</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>search</v-icon>
+            </v-btn>
+          </v-toolbar>
+          <v-container fluid grid-list-md class="grey lighten-4">
+            <v-layout row>
+             <v-flex xs12 sm12>
+              <v-card height="200px">
+                <div class="headline text-xs-center pa-5">Active: {{ e1 }}</div>
+                <v-bottom-nav absolute :value="true" :active.sync="e1" class="transparent">
+                  <v-btn flat color="teal" value="recent">
+                    <span>Recent</span>
+                    <v-icon>history</v-icon>
+                  </v-btn>
+                  <v-btn flat color="teal" value="favorites">
+                    <span>Favorites</span>
+                    <v-icon>favorite</v-icon>
+                  </v-btn>
+                  <v-btn flat color="teal" value="nearby">
+                    <span>Nearby</span>
+                    <v-icon>place</v-icon>
+                  </v-btn>
+                </v-bottom-nav>
+              </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Main',
+  data () {
+    return {
+      e1: 'recent'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+</style>
--- mc-front/src/router/index.js
+++ mc-front/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/components/Main'
+import Vuetify from 'vuetify'
 
 Vue.use(Router)
+Vue.use(Vuetify)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
     }
   ]
 })
